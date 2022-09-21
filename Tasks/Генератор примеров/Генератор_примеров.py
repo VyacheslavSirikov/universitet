ccaf7c8af2ddab8b2e3c8c2ca92510cc5ecb6b0d
VyacheslavSirikov <s.sirikov28@gmail.com>
2022-09-22 00:57:10 +0300

    new prog

---
+++ Tasks/Генератор примеров/Генератор_примеров.py
@@ -0,0 +1,46 @@
+import random
+
+a = 1
+while a == 1:
+
+    lives = 3
+    print('Кол-во жизней = ', lives)
+
+    d = random.randint(0, 1000)
+    c = random.randint(0, 1000)
+    r = random.randint(0, 1)
+    o = 0
+    f = 0
+    if r == 0:
+        f = ' - '
+        o = d - c
+    else:
+        f = ' + '
+        o = d + c
+
+    while lives > 0:
+
+        if lives == 3:
+            m = 'Решите пример: '
+        if lives != 3:
+            m = 'Попробуйте еще раз: '
+
+        print(m, d, f, c)
+        op = int(input())
+
+        if op == o:
+            print('Вы выиграли')
+            break
+        if op != o:
+            lives = lives - 1
+            if lives == 0:
+                print('Вы проиграли')
+                break
+            print('Вы ошиблись. Количество жизней = ', lives, )
+
+    print('Если хотите продолжить введите 1, если нет 0')
+    p = int(input())
+    if p == 1:
+        a = 1
+    else:
+        a = 0
