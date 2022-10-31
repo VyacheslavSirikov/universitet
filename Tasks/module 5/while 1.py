0634ea65c00ad71b123eb101776820d7219d5a83
VyacheslavSirikov <s.sirikov28@gmail.com>
2022-10-31 23:00:42 +0300

    new module

---
+++ Tasks/module 5/while 1.py
@@ -0,0 +1,7 @@
+n = int(input())
+div = 2
+
+while n % div != 0:
+    div += 1
+
+print(div)
---
+++ Tasks/module 5/while 3.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+
+days = 1
+
+while x <= y:
+    x += x/10
+    days += 1
+
+print(days)
